351c9bf42aef8c2b8701bcc0e4670eabeb51d0a2
BiancaGiovanna <biancagothicg@gmail.com>
2024-08-08 13:50:27 -0300

    first commit

---
+++ src/main/java/com/example/api/demoapirest/DemoApiRestApplication.java
@@ -0,0 +1,15 @@
+package com.example.api.demoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class DemoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiRestApplication.class, args);
+		System.out.print("here");
+	}
+
+}
